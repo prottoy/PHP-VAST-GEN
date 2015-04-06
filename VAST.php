0caf82e56616755540ffb9b8d9c79a33c54634ad
prottoy <prottoy2007@gmail.com>
2015-04-06 16:16:44 +0600

    appendChild() and addAttributes() method created

--- VAST.php
+++ VAST.php
@@ -13,64 +13,40 @@ class VAST {
         header("Content-type: text/xml");
         $doc->formatOutput = true;
 
-        //<VAST version="3.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="vast.xsd">
+        
+        //Vast header
         $root = $doc->createElement("VAST");
         $doc->appendChild($root);
+        $vastAttributes=array('version'=>'3.0', 'xmlns:xsi'=>'http://www.w3.org/2001/XMLSchema-instance','xsi:noNamespaceSchemaLocation'=> 'vast.xsd'); 
+        $this->addAttributes($doc, $root, $vastAttributes);
 
-        // create attribute version
-        $version = $doc->createAttribute("version");
-        $version->value = '3.0';
-        $root->appendChild($version);
 
-        // create attribute version
-        $xsi = $doc->createAttribute("xmlns:xsi");
-        $xsi->value = 'http://www.w3.org/2001/XMLSchema-instance';
-        $root->appendChild($xsi);
-
-        // create attribute noNamespaceSchemaLocation
-        $xsi_no = $doc->createAttribute("xsi:noNamespaceSchemaLocation");
-        $xsi_no->value = 'vast.xsd';
-        $root->appendChild($xsi_no);
-
-        // <Ad id="preroll-1" sequence="1">
-        $ad = $doc->createElement("Ad");
-        // create attribute ad id
-        $ad_id = $doc->createAttribute("id");
-        $ad_id->value = 'preroll-1';
-        $ad->appendChild($ad_id);
-
-        // create attribute ad sequence
-        $ad_sequence = $doc->createAttribute("sequence");
-        $ad_sequence->value = '1';
-        $ad->appendChild($ad_sequence);
-
+        //ad header
+        $ad = $doc->createElement("Ad");        
+        $adAttributes=array('id'=>'preroll-1','sequence'=>'1'); 
+        $this->addAttributes($doc, $ad, $adAttributes);
+        
+       
         $inLine = $doc->createElement("InLine");
         $adSystem = $doc->createElement("AdSystem");
-
-        // create attribute ad sequence
-        $adSystem_ver = $doc->createAttribute("version");
-        $adSystem_ver->value = '2.0';
-        $adSystem->appendChild($adSystem_ver);
+        $adsystemAttributes= array('version'=>'2.0');
+        $this->addAttributes($doc, $adSystem, $adsystemAttributes);
 
 
         $adTitle = $doc->createElement("AdTitle");
         $description = $doc->createElement("Description");
         $survey = $doc->createElement("Survey");
+        
         $impression = $doc->createElement("Impression");
+        $impressionAttributes= array('id'=>'DART');
+        $this->addAttributes($doc, $impression, $impressionAttributes);
+        
         $creatives = $doc->createElement("Creatives");
 
         $this->generateCreatives($doc, $creatives);
-        // create attribute ad sequence
-        $impression_id = $doc->createAttribute("id");
-        $impression_id->value = 'DART';
-        $impression->appendChild($impression_id);
-
-        $inLine->appendChild($adSystem);
-        $inLine->appendChild($adTitle);
-        $inLine->appendChild($description);
-        $inLine->appendChild($survey);
-        $inLine->appendChild($impression);
-        $inLine->appendChild($creatives);
+        
+        $childs= array($adSystem, $adTitle, $description, $survey, $impression, $creatives);        
+        $this->appendChilds($inLine, $childs);
 
         $ad->appendChild($inLine);
         $root->appendChild($ad);
@@ -178,6 +154,12 @@ class VAST {
             $parent->appendChild($attr);
         }
             
+    }
+    
+    public function appendChilds($parent,$childs){
+        foreach ($childs as $child) {
+            $parent->appendChild($child);
+        }
     }
 
 }
